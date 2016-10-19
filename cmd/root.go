b98f8f245273cf89cd3496e5b872058bad236571
Grayson Chao <grayson.chao@gmail.com>
2016-10-19 16:17:25 -0700

    Set up command line + config parsing

---
+++ cmd/root.go
@@ -0,0 +1,75 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "udpmux",
+	Short: "Multiplex a UDP stream across several sources.",
+	Long:  `udpmux multiplexes a UDP stream across several sources.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg.Port = viper.GetInt("port")
+		cfg.Debug = viper.GetBool("debug")
+		cfg.Outputs = viper.GetStringMap("outputs")
+
+		if cfg.Debug {
+			fmt.Println(cfg)
+		}
+
+		//udpmux(cfg)
+	},
+}
+
+var cfg = struct {
+	Outputs map[string]interface{}
+	Port    int
+	Debug   bool
+}{}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports Persistent Flags, which, if defined here,
+	// will be global for your application.
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.udpmux.yaml)")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	viper.SetConfigName(".udpmux") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")   // adding home directory as first search path
+	viper.AutomaticEnv()           // read in environment variables that match
+
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Println(err)
+	}
+}
